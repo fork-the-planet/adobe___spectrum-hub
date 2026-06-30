1b2cde0a61e3f2a2ce5a90f866b79a2051e78d87
marissahuysentruyt <marissa.huysentruyt@heysparkbox.com>
2026-06-30 14:38:35 -0400

    refactor(footer): use tokens but not BEM
    reverts BEM naming changes in 4c24b711946c9ed1f549056982fdec25ae719b46

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -8,22 +8,21 @@ const FOOTER_PATH = '/fragments/nav/footer';
  * @param {Element} el The footer element
  */
 export default async function init(el) {
-  el.classList.add('footer');
   const { locale } = getConfig();
   const footerMeta = getMetadata('footer');
   const path = footerMeta || FOOTER_PATH;
 
   const { fragment } = await loadFragment(`${locale.prefix}${path}`);
   if (!fragment) { return; }
-  fragment.classList.add('footer__content');
+  fragment.classList.add('footer-content');
 
   const sections = [...fragment.querySelectorAll('.section')];
 
   const copyright = sections.pop();
-  copyright.classList.add('footer__copyright');
+  copyright.classList.add('section-copyright');
 
   const legal = sections.pop();
-  legal.classList.add('footer__legal');
+  legal.classList.add('section-legal');
 
   el.append(fragment);
 }
